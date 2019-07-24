935078d86308ef03fc114a833808839931d6bf84
Victor Jian Choe <vchoe89@gmail.com>
2019-07-24 21:22:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -30,13 +30,8 @@ def my_hash_creator(key, value)
   }
 end
 
-
 def read_from_hash(hash, key)
-  if hash[key]
-    hash[key] += 1
-  else
-    hash[key] = 1
-  end
+    hash[key]
 end
 
 def update_counting_hash(hash, key)
