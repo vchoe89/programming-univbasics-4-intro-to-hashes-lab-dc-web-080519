44b6d3ce0a648fb42958cb5819c5f4874a3437d6
Victor Jian Choe <vchoe89@gmail.com>
2019-07-24 19:50:58 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,21 +1,32 @@
 def new_hash
-  # return an empty hash
+  new_hash = {}
+  return new_hash
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  cat_color = {
+    cat: "oliver",
+    color: "orange"
+  }
 end
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  pioneer = {
+    name: "Grace Hopper"
+  }
 end
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  id_generator = {
+    id: 5
+  }
 end
 
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  name = {
+    :name => "Grace Hopper"
+  }
+  name
 end
 
 def read_from_hash(hash, key)
